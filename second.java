36057ccd577cebec4397e7eb88b223e66a5544dd
Anand T P <37290770+anandtpradeep@users.noreply.github.com>
2024-07-29 11:01:32 +0530

    Update and rename first.jav to second.java

--- second.java
+++ second.java
@@ -1,5 +1,8 @@
-public static void main(String args[] )
+class New
+{
+  public static void main(String args[] )
 {
 System.out.println("Welcome to my new branch");
 System.out.printlm("Commit through github");
+}
 }
