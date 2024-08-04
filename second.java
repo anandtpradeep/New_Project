e8e75ad5d3acd055e0a63698c9e271cf322f2af5
anandtpradeep <anandtpradeep@gmail.com>
2024-08-04 17:38:33 +0000

    commiting changes in branch1

--- second.java
+++ second.java
@@ -2,7 +2,7 @@ class New
 {
   public static void main(String args[] )
 {
-System.out.println("Welcome to my new branch");
+System.out.println("Welcome to my new second branch");
 System.out.printlm("Commit through github");
 }
 }
